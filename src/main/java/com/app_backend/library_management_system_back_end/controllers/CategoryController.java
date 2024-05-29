827feaf035df56635eaaede507e30adb3c916b32
alrhazes <anuuman2@gmail.com>
2024-05-29 12:36:51 +0100

    commit

--- src/main/java/com/app_backend/library_management_system_back_end/controllers/CategoryController.java
+++ src/main/java/com/app_backend/library_management_system_back_end/controllers/CategoryController.java
@@ -1,2 +1,11 @@
-package com.app_backend.library_management_system_back_end.controllers;public class CategoryController {
+package com.app_backend.library_management_system_back_end.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/category")
+
+public class CategoryController {
 }
